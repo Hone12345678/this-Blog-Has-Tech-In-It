152f8c45fdcb25f81dc8418c281acb0f4bdd9897
CapNemo <46331608+Hone12345678@users.noreply.github.com>
2022-03-05 13:30:37 -0700

    revising and debuging comments

---
+++ public/javascript/comment.js
@@ -0,0 +1,32 @@
+async function commentFormHandler(event) {
+    event.preventDefault();
+    console.log(event);
+  
+    const comment_text = document.querySelector('textarea[name="comment-body"]').value.trim();
+    const post_id = window.location.toString().split('/')[
+      window.location.toString().split('/').length - 1
+    ];
+  console.log(comment_text, post_id);
+    if (comment_text) {
+      const response = await fetch('/api/comments', {
+        method: 'POST',
+        body: JSON.stringify({
+          post_id,
+          comment_text
+        }),
+        headers: {
+          'Content-Type': 'application/json'
+        }
+      });
+  
+      if (response.ok) {
+        console.log("nailed it");
+        document.location.reload();
+      } else {
+        alert(response.statusText);
+      }
+    }
+  }
+  
+  document.querySelector('.comment-form').addEventListener('submit', commentFormHandler);
+  
